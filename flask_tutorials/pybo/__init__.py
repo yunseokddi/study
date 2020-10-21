00ee95eab704da222cab2d6954b6b43ddd867913
oys5339 <oys5339@naver.com>
2020-10-21 17:33:33 +0900

    flask study update

--- flask_tutorials/pybo/__init__.py
+++ flask_tutorials/pybo/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
 
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/')
-    def hello_pybo():
-        return 'Hello, Pybo!'
+    app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import models
+
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
 
     return app
--- flask_tutorials/pybo/views/main_views.py
+++ flask_tutorials/pybo/views/main_views.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/')
+@bp.route('/hello')
 def hello_pybo():
     return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return 'Pybo index'
